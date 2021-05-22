cbc5fbc820d687210c2792e59e8ca2ed53421fbe
AboodIbrahim <aboodibrahim50@gmail.com>
2021-05-22 17:17:40 +0200

    finished styling

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let inputWrappers = document.querySelectorAll('.input-wrapper');
+
+let icons = {
+    email: 'email',
+    phone: 'phone',
+    name: 'account_circle',
+    address: 'home',
+    city: 'location_city',
+    country: 'location_city',
+    postalCode: 'markunread_mailbox',
+};
+
+inputWrappers.forEach(e => {
+    e.dataset.icon = icons[e.children[0].name];
+});
